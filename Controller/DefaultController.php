e846915e7c5b2ac6c64650d6df114824847d13d4
sturple <shawn.turple@shaw.ca>
2016-04-15 09:56:29 -0700

    Updated DefaultController.php * Added logic to flatten configurations. * Added config into twig template variables. * Added Errors twig template varaibles.
    Updated routing to annotation.
    Removed Resources/config/routing.yml
    
    Added resources for drumjs
    Updated digital comment card template to add drumjs resources
    
    Updated data-sample.yml to add configurations and to remove last entry
    on standard question set.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,20 +3,57 @@
 namespace FanFerret\DevelopmentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Parser;
 
 class DefaultController extends Controller
 {
+	/**
+	 * @Route("/", name="fan_ferret_development_homepage" )
+	 *
+	 **/
     public function indexAction()
     {
         return $this->render('FanFerretDevelopmentBundle:Default:index.html.twig');
     }
-
+	
+	/**
+	 * @Route("/template/{twigname}", name="fan_ferret_development_template" )
+	 *
+	 **/
     public function templateAction($twigname="index")
     {
         $param = $this->getYaml(__DIR__ ."/../Tests/data-sample.yml");
 		$param =  ($param === null) ? array() : $param;
+		
+		// flatten out configurations
+		if (is_array($param['configuration'])) {
+			$count = 1;
+			foreach ($param['configuration'] as $config){
+				if (isset($config['type']) && isset($config['name']) && isset($config['value'])){
+					$typeFlag = preg_match('/(css|image|template)$/', $config['type'], $mtype);
+					$nameFlag = preg_match('/^[a-z]\w+$/', $config['name'], $mname);
+					if( $typeFlag  && $nameFlag ){
+						$param['config'][$config['type']][$config['name']] = $config['value'];
+					}
+					else {
+						$message = 'Item: '. $count .' Error parsing configuration. ';
+						$message .= $typeFlag ? '' : 'Property type can only be {css|image|template}. ';
+						$message .= $nameFlag ? '' : 'Property name has to start with lowercase, and can only be alphanumeric. ';
+						$param['errors'][] = $message . json_encode($config);
+					}
+					
+				}
+				else {
+					$message = 'Item: '. $count . ' Error parsing configuration.  Require fields type, name, and value';
+					$param['errors'][] = $message;
+				}
+				++$count;
+				
+			}
+		}
+		
         return $this->render('FanFerretDevelopmentBundle:Default:'. $twigname .'.html.twig',$param);
     }
 
@@ -24,6 +61,7 @@ class DefaultController extends Controller
 	 * reads yaml files
 	 *
 	 * @param string filename
+	 * 
 	 */
 	private function getYaml($filename)
 	{
